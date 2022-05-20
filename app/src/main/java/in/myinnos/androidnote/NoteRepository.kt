676f90ae22d63d1707df660edf9d18f5df2fff20
Prabhakar Thota <prabhakar.s@1bridge.one>
2022-05-20 16:19:24 +0530

    creating a Repository class

---
+++ app/src/main/java/in/myinnos/androidnote/NoteRepository.kt
@@ -0,0 +1,25 @@
+package `in`.myinnos.androidnote
+
+import androidx.lifecycle.LiveData
+
+class NoteRepository(private val notesDao: NotesDao) {
+
+    val allNotes: LiveData<List<Note>> = notesDao.getAllNotes()
+
+    // insert
+    suspend fun insert(note: Note) {
+        notesDao.insert(note)
+    }
+
+    //delete
+    suspend fun delete(note: Note){
+        notesDao.delete(note)
+    }
+
+    // update
+    suspend fun update(note: Note){
+        notesDao.update(note)
+    }
+
+
+}
--- app/src/main/java/in/myinnos/androidnote/NotesDao.kt
+++ app/src/main/java/in/myinnos/androidnote/NotesDao.kt
@@ -8,23 +8,21 @@ abstract class NotesDao {
 
     // to insert data
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(note: Note) {
+    suspend fun insert(note: Note) {
     }
 
     // to delete data
     @Delete
-    fun delete(note: Note) {
+    suspend fun delete(note: Note) {
     }
 
     // to update data
     @Update
-    fun update(note: Note) {
+    suspend fun update(note: Note) {
     }
 
     // to read the data (all the data)
     @Query("Select * from notesTable order by id ASC")
-    //fun getAllNotes(): LiveData<List<Note>>
-    fun getAllNotes() {
-    }
+    abstract fun getAllNotes(): LiveData<List<Note>>
 
 }
