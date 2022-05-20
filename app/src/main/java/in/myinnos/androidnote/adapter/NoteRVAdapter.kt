461a9464b1884e86371e6ae0b46d631b3f9704ab
Prabhakar Thota <prabhakar.s@1bridge.one>
2022-05-20 17:41:43 +0530

    creating a adapter class

--- app/src/main/java/in/myinnos/androidnote/adapter/NoteRVAdapter.kt
+++ app/src/main/java/in/myinnos/androidnote/adapter/NoteRVAdapter.kt
@@ -1,4 +1,70 @@
 package `in`.myinnos.androidnote.adapter
 
-class NoteRVAdapter {
+import `in`.myinnos.androidnote.R
+import `in`.myinnos.androidnote.database.Note
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRVAdapter(
+    val context: Context, private val noteClickDeleteInterface: NoteClickDeleteInterface,
+    private val noteClickInterface: NoteClickInterface
+) :
+    RecyclerView.Adapter<NoteRVAdapter.ViewHolder>() {
+
+    // variables
+    private val allNotes = ArrayList<Note>()
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteTV: TextView = itemView.findViewById(R.id.idTVNote)
+        val dateTV: TextView = itemView.findViewById(R.id.idTVDate)
+        val deleteIV: ImageView = itemView.findViewById(R.id.idIVDelete)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteRVAdapter.ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.note_rv_item,
+            parent, false
+        )
+        return ViewHolder(itemView)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: NoteRVAdapter.ViewHolder, position: Int) {
+        holder.noteTV.text = allNotes.get(position).noteTitle
+        holder.dateTV.text = "Last Updated : " + allNotes[position].timestamp
+
+        holder.deleteIV.setOnClickListener {
+            noteClickDeleteInterface.onDeleteIconClick(allNotes[position])
+        }
+
+        holder.itemView.setOnClickListener {
+            noteClickInterface.onNoteClick(allNotes[position])
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateList(newList: List<Note>) {
+        allNotes.clear()
+        allNotes.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
+
+interface NoteClickDeleteInterface {
+    fun onDeleteIconClick(note: Note)
+}
+
+interface NoteClickInterface {
+    fun onNoteClick(note: Note)
 }
